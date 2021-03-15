b2c343452f69fe85e7956433401ed8cb0d19d7e4
moacyr.trombetta <moacyr.trombetta@cwi.com.br>
2021-03-14 23:27:05 -0300

    add models

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,9 +1,32 @@
 import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository'
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.get('/', (request, response) => {
-    return response.json( {message: 'Hello World'})
+    return response.json({ message: 'Hello World' })
+});
+
+appointmentsRouter.post('/', (request, response) => {
+    const { provider, date } = request.body;
+
+    const parsedDate = startOfHour(parseISO(date));
+
+    const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
+
+    if (findAppointmentInSameDate) {
+        return response.status(400).json({ message: 'This appointment is already booked!' })
+    }
+
+    const appointment = appointmentsRepository.create({
+        provider,
+        date: parsedDate,
+    });
+
+    return response.json(appointment);
 });
 
 export default appointmentsRouter;
