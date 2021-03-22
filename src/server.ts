31216a5daf6277fe787e397299929bb9e50f206b
Moacyr Trombetta <moacyr.trombetta@cwi.com.br>
2021-03-22 08:50:11 -0300

    add migrations

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
-import express from 'express';
-import routes from './routes'
+import 'reflect-metadata';
 
+import express from 'express';
+import routes from './routes';
 import './database';
 
 const app = express();
 
 app.use(express.json());
-app.use(routes)
+app.use(routes);
 
 app.listen(3333, () => {
     console.log('API STARTED 🚀')
